0fb5f6c566510d8c649ccfdeb654193003d93087
ajinkya1985 <ajinkya.chinchwadkar@gmail.com>
2023-04-11 22:27:47 +0530

    New machine learning model

---
+++ ML_model.py
@@ -0,0 +1,14 @@
+def model 1:
+    print("Model 1 is running")
+    return model 1
+
+def model 2:
+    print("Model 2 is running")
+    return model 2
+
+def model 3:
+    print("Model 3 is running")
+    return model 3
+
+print("Model 2 is optimum")
+
